829500f5d5197a4d3eded6b479cb70367923dc00
Ross McKay <rmckay@webaware.com.au>
2020-06-24 15:10:49 +1000

    compile JS and bump version

--- facebook-video-block.php
+++ facebook-video-block.php
@@ -3,13 +3,13 @@
 Plugin Name: Facebook Video Block
 Plugin URI: https://github.com/webaware/fb-video-block
 Description: a Gutenberg block for adding Facebook videos to a page/post
-Version: 0.0.3
+Version: 0.0.4
 Author: WebAware
 Author URI: https://shop.webaware.com.au/
 */
 
 /*
-copyright (c) 2018 WebAware Pty Ltd (email : support@webaware.com.au)
+copyright (c) 2018-2020 WebAware Pty Ltd (email : support@webaware.com.au)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -34,8 +34,8 @@ define('FB_VIDEO_BLOCK_PLUGIN_FILE', __FILE__);
 define('FB_VIDEO_BLOCK_PLUGIN_ROOT', __DIR__ . '/');
 define('FB_VIDEO_BLOCK_PLUGIN_NAME', basename(__DIR__) . '/' . basename(__FILE__));
 
-define('FB_VIDEO_BLOCK_MIN_PHP', '5.4');
-define('FB_VIDEO_BLOCK_VERSION', '0.0.3');
+define('FB_VIDEO_BLOCK_MIN_PHP', '7.2');
+define('FB_VIDEO_BLOCK_VERSION', '0.0.4');
 
 if (version_compare(PHP_VERSION, FB_VIDEO_BLOCK_MIN_PHP, '<')) {
 	return;
