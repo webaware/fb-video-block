c8bd49305a235cb2ec8dda680527956e5687bb3a
Ross McKay <rmckay@webaware.com.au>
2020-06-24 15:17:16 +1000

    load FB in en_GB locale

--- views/script-block.php
+++ views/script-block.php
@@ -7,4 +7,5 @@ if (!defined('ABSPATH')) {
 ?>
 
 <div id="fb-root"></div>
-<script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
+<script async defer src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2"></script>
+
